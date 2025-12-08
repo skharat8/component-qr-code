0f53b1fb6e4f41727c9681fcfa4f48ac3966ac79
skharat8 <5607079+skharat8@users.noreply.github.com>
2025-12-08 00:03:37 -0800

    Add global styles, fonts, and image assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import QRCodeImage from "./assets/image-qr-code.png";
 import "./App.css";
 
 function App() {
-  return <>Project setup</>;
+  return (
+    <>
+      <div className="wrapper">
+        <img src={QRCodeImage} />
+        <h1 className="title">
+          Improve your front-end skills by building projects
+        </h1>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default App;
