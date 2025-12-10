f92c902622d4de7c2b0df8c08209526049d9f49f
skharat8 <5607079+skharat8@users.noreply.github.com>
2025-12-10 14:26:13 -0800

    Minor improvements with margin and padding

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import "./App.css";
 
 function App() {
   return (
-    <QRCode>
-      <h1 className="title">
-        Improve your front-end skills by building projects
-      </h1>
-      <p>
-        Scan the QR code to visit Frontend Mentor and take your coding skills to
-        the next level
-      </p>
-    </QRCode>
+    <main>
+      <QRCode>
+        <h1 className="title">
+          Improve your front-end skills by building projects
+        </h1>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </QRCode>
+    </main>
   );
 }
 
--- src/QRCode.tsx
+++ src/QRCode.tsx
@@ -4,7 +4,7 @@ import styles from "./QRCode.module.css";
 function QRCode({ children }: React.PropsWithChildren) {
   return (
     <div className={styles.wrapper}>
-      <img src={QRCodeImage} className={styles.image} />
+      <img src={QRCodeImage} className={styles.image} alt="QR Code" />
       <div className={styles.textWrapper}>{children}</div>
     </div>
   );
