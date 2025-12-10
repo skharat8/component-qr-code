574a585c3c1a25723a03efc6ace32ad86abd09e8
skharat8 <5607079+skharat8@users.noreply.github.com>
2025-12-10 12:27:52 -0800

    Create QR Code component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import QRCodeImage from "./assets/image-qr-code.png";
+import QRCode from "./QRCode";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="wrapper">
-        <img src={QRCodeImage} />
-        <h1 className="title">
-          Improve your front-end skills by building projects
-        </h1>
-        <p>
-          Scan the QR code to visit Frontend Mentor and take your coding skills
-          to the next level
-        </p>
-      </div>
-    </>
+    <QRCode>
+      <h1 className="title">
+        Improve your front-end skills by building projects
+      </h1>
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </QRCode>
   );
 }
 
---
+++ src/QRCode.tsx
@@ -0,0 +1,13 @@
+import QRCodeImage from "./assets/image-qr-code.png";
+import styles from "./QRCode.module.css";
+
+function QRCode({ children }: React.PropsWithChildren) {
+  return (
+    <div className={styles.wrapper}>
+      <img src={QRCodeImage} className={styles.image} />
+      <div className={styles.textWrapper}>{children}</div>
+    </div>
+  );
+}
+
+export default QRCode;
